887d030951702bf90b0eb29c0296356f6d3ade78
me <me@gmail.com>
2023-11-28 17:45:20 +0100

    first commit: Pipeline Configration

---
+++ BackEnd - Server - MVC/src/services/mongo.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose")
+
+require('dotenv').config();
+
+const MONGO_URL = process.env.MONGO_URL;
+
+//Event Emitter for mongodb connection once
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection ready!')
+});
+//Event Emitter for mongodb connection on error
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
+
+//Create a mangoConnect function to export that calls mngoose.connect
+async function mongoConnect() {
+    await mongoose.connect(MONGO_URL, {
+        useUnifiedTopology: true
+    }); 
+};
+
+async function mongoDisconnect() {
+    await mongoose.disconnect();
+}
+
+module.exports = {
+    mongoConnect,
+    mongoDisconnect
+}
